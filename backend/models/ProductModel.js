df5c844440743fb20af68e76eab9c2f6a253b22d
YANZ <107906629+Yayanfr20@users.noreply.github.com>
2023-06-06 13:54:07 +0700

    Add Backend

---
+++ backend/models/ProductModel.js
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const Product = db.define(
+  "product",
+  {
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    url: DataTypes.STRING,
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+export default Product;
+
+(async () => {
+  await db.sync();
+})();
